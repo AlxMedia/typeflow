2c2db49d98a92f826e7578a0f2f7cd33fcddd109
Alexander Agnarson <contact@alx.media>
2024-04-07 16:07:02 +0200

    Add comment count to large featured entries

--- content-featured-large.php
+++ content-featured-large.php
@@ -11,8 +11,14 @@
 			
 			</a>
 			
-			<h2 class="feaured-large-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+			<h2 class="featured-large-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 			<div class="featured-large-date"><?php the_time( get_option('date_format') ); ?></div>
+			
+			<?php if ( comments_open() && ( get_theme_mod( 'comment-count', 'on' ) =='on' ) ): ?>
+				<?php $number = get_comments_number( $post->ID ); if ( $number > 0 ) { ?>
+					<a class="card-comments" href="<?php comments_link(); ?>"><strong><i class="far fa-comment-alt"></i><span><?php comments_number( '0', '1', '%' ); ?></span></strong></a>
+				<?php } ?>
+			<?php endif; ?>
 		
 	</div>
 </div>
